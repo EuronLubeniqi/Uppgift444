161d478440ca9306bf895f21331ea4cb62803281
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-13 09:02:00 +0100

    Add project files.

---
+++ Uppgift444/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string password;
+
+        do
+        {
+            Console.Write("Ange lösenord: ");
+            password = Console.ReadLine();
+        } while (password != "hemligt");
+
+        Console.WriteLine("Du angav rätt lösenord!");
+    }
+}
+
